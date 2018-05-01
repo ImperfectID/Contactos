169ad91f2d79c2a9222480c1965ef3f69f7d04b3
ImperfectID <00172216@uca.edu.sv>
2018-05-01 14:53:32 -0600

    RECYCLE_LAYOUT

--- app/src/main/java/com/example/rodri/contactos/MainActivity.java
+++ app/src/main/java/com/example/rodri/contactos/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.rodri.contactos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+    ArrayList<String> lista_contactos;
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.rv_contactos);
+        rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        lista_contactos=new ArrayList<String>();
+
+        for(int i=0;i<=50;i++){
+            lista_contactos.add("Dato:"+i+"");
+
+        }
+        adapter ad = new adapter(lista_contactos);
+        rv.setAdapter(ad);
+
     }
 }
--- app/src/main/java/com/example/rodri/contactos/adapter.java
+++ app/src/main/java/com/example/rodri/contactos/adapter.java
@@ -1,4 +1,49 @@
 package com.example.rodri.contactos;
 
-public class adapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class adapter extends RecyclerView.Adapter<adapter.ViewHolderDatos> {
+    ArrayList<String> lista_contactos;
+
+    public adapter(ArrayList<String> lista_contactos){
+        this.lista_contactos = lista_contactos;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.lista,null,false);
+        return new ViewHolderDatos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
+            holder.asignardatos(lista_contactos.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return lista_contactos.size();
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+
+        TextView dato;
+        public ViewHolderDatos(View itemView) {
+            super(itemView);
+            dato = itemView.findViewById(R.id.lista);
+        }
+
+        public void asignardatos(String s) {
+            dato.setText(s);
+        }
+    }
 }
